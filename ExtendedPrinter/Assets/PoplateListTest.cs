5a042c10613481a906f075db51ac17b87491682b
Florian Jasche <florian.jasche@gmail.com>
2020-12-06 16:30:05 +0100

    created a scrollable view for selecting models

---
+++ ExtendedPrinter/Assets/PoplateListTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.MixedReality.Toolkit.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoplateListTest : MonoBehaviour
+{
+    public ScrolinglListContentController scrollView;
+
+    public GameObject ListItemPrefab;
+
+   [ContextMenu("Test")]
+    void Test()
+    {
+        var listItem = Instantiate(ListItemPrefab);
+
+        scrollView.AddContent(listItem);
+    }
+
+    
+}
