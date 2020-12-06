ba93473737d78df22ac0ecf81ab91f2df9d4780b
Florian Jasche <florian.jasche@gmail.com>
2020-12-06 03:45:03 +0100

    created localized button
    imported text mesh pro

---
+++ ExtendedPrinter/Assets/_ExtendedPrinter/Scripts/ButtonLocalization.cs
@@ -0,0 +1,58 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.Localization;
+using System;
+
+public class ButtonLocalization : MonoBehaviour
+{
+    [SerializeField]
+    private LocalizedString stringRefButton;
+    [SerializeField]
+    private LocalizedString stringRefLabel;
+
+    [SerializeField]
+    private TextMeshPro textMeshButton;
+    [SerializeField]
+    private TextMeshPro textMeshLabel;
+
+
+    void OnEnable()
+    {
+        if(stringRefButton == null)
+        {
+            throw new ArgumentNullException(nameof(stringRefButton));
+        }
+        if (textMeshButton == null)
+        {
+            throw new ArgumentNullException(nameof(textMeshButton));
+        }
+        if (stringRefLabel == null)
+        {
+            throw new ArgumentNullException(nameof(stringRefLabel));
+        }
+        if (textMeshLabel == null)
+        {
+            throw new ArgumentNullException(nameof(textMeshLabel));
+        }
+
+        stringRefButton.StringChanged += UpdateString(textMeshButton);
+        stringRefLabel.StringChanged += UpdateString(textMeshLabel);
+    }
+
+    void OnDisable()
+    {
+        stringRefButton.StringChanged -= UpdateString(textMeshButton);
+        stringRefLabel.StringChanged -= UpdateString(textMeshLabel);
+    }
+
+    LocalizedString.ChangeHandler UpdateString(TextMeshPro textMesh)
+    {
+        Action<string> action = (translatedValue) =>
+        {
+            var _textMesh = textMesh;
+            _textMesh.text = translatedValue;
+        };
+
+        return new LocalizedString.ChangeHandler(action);
+    }
+}
