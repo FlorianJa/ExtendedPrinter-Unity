6e6d67d1dcf6055f248375ee7380f62bbe56761a
Florian Jasche <->
2020-12-09 19:11:09 +0100

    Added default material to STLImporter

--- ExtendedPrinter/Assets/_ExtendedPrinter/Scripts/STLImporter.cs
+++ ExtendedPrinter/Assets/_ExtendedPrinter/Scripts/STLImporter.cs
@@ -6,20 +6,30 @@ using UnityEngine;
 
 public class STLImporter : MonoBehaviour
 {
-    public void Import(string stlPath, Transform parent)
+
+    public Material DefaultMaterial;
+
+    public void Import(string stlPath, Transform parent, bool smooth = true)
     {
         if (!string.IsNullOrEmpty(stlPath) && File.Exists(stlPath))
         {
+
+
             var meshes = Importer.Import(stlPath, CoordinateSpace.Right, UpAxis.Y, true);
 
             foreach (var mesh in meshes)
             {
                 var tmp = new GameObject();
                 tmp.AddComponent<MeshFilter>().mesh = mesh;
-                tmp.AddComponent<MeshRenderer>();//.material = material;
+                var renderer = tmp.AddComponent<MeshRenderer>();
+                if(DefaultMaterial != null)
+                {
+                    renderer.material = DefaultMaterial;
+                }
                 tmp.transform.localScale = new Vector3(0.001f, 0.001f, 0.001f);
                 tmp.transform.SetParent(parent);
             }
         }
     }
+
 }
