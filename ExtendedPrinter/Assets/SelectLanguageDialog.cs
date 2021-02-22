31d8d468a20f8a7ebb9f01f799dff3a20fd34d47
Florian Jasche <florian.jasche@gmail.com>
2021-02-22 19:22:41 +0100

    WIP: creating Language selection (needs MRTK update)

---
+++ ExtendedPrinter/Assets/SelectLanguageDialog.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.MixedReality.Toolkit.Experimental.Dialog;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectLanguageDialog : Dialog
+{
+
+    private SelectLanguageDialogButton[] Buttons;
+
+    private GameObject[] twoButtonSet;
+
+
+    protected override void FinalizeLayout()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void GenerateButtons()
+    {
+        foreach (var button in Buttons)
+        {
+            button.ParentDialog = this;
+        }
+    }
+
+    protected override void SetTitleAndMessage()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ ExtendedPrinter/Assets/SelectLanguageDialogButton.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.MixedReality.Toolkit.Experimental.Dialog;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectLanguageDialogButton : DialogButton
+{
+    [SerializeField]
+    private string Locale;
+
+    /// <summary>
+    /// Event handler that runs when button is clicked.
+    /// Dismisses the parent dialog.
+    /// </summary>
+    /// <param name="obj">Caller GameObject</param>
+    public new void OnButtonClicked(GameObject obj)
+    {
+        if (ParentDialog != null)
+        {
+            ParentDialog.Result.Result = ButtonTypeEnum;
+            ParentDialog.Result.Variable = Locale;
+            ParentDialog.DismissDialog();
+        }
+    }
+}
