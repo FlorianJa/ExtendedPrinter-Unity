67fd3a18b2777c7f978d4fd5966fe1984b4beeb9
Florian Jasche <florian.jasche@gmail.com>
2021-02-27 22:34:21 +0100

    Added ClippingPlane for GCode View

--- ExtendedPrinter/Assets/_ExtendedPrinter/Scripts/GCodeController.cs
+++ ExtendedPrinter/Assets/_ExtendedPrinter/Scripts/GCodeController.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections;
+﻿using Microsoft.MixedReality.Toolkit.UI;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,6 +16,10 @@ public class GCodeController : MonoBehaviour
     [SerializeField]
     private Material SupportMaterial;
 
+    [SerializeField]
+    private ClippingPlane clippingPlane;
+
+    private float maxHeight = 0;
     public void RemoveChildren()
     {
         while (transform.childCount > 0)
@@ -48,5 +55,59 @@ public class GCodeController : MonoBehaviour
                 }
             }
         }
+    }
+
+    public void AddRenderersToClippingPlane(bool clearRenderersBefore)
+    {
+        if(clearRenderersBefore)
+        {
+            clippingPlane.ClearRenderers();
+        }
+
+        var renderers = GetComponentsInChildren<Renderer>();
+
+        foreach (Renderer renderer in renderers)
+        {
+            clippingPlane.AddRenderer(renderer);
+        }
+
+        SetClippingPlaneToMaxYPosition();
+    }
+
+    private void SetClippingPlaneToMaxYPosition()
+    {
+        //TODO: check performance!
+        foreach (Transform child in transform)
+        {
+            if (child.gameObject.name == "Perimeter")
+            {
+                var meshFilters = child.gameObject.GetComponentsInChildren<MeshFilter>();
+
+                foreach (var meshFilter in meshFilters)
+                {
+                    foreach(var vertex in meshFilter.mesh.vertices)
+                    {
+                        if (maxHeight < vertex.y)
+                        {
+                            maxHeight = vertex.y;
+                        }
+                    }
+                }
+            }
+        }
+        maxHeight = maxHeight / 1000f;
+
+        var newPosition = clippingPlane.transform.localPosition;
+        newPosition.y = maxHeight;
+        clippingPlane.transform.localPosition = newPosition;
+        
+    }
+
+    public void OnSliderValueChanged(SliderEventData sliderEventData)
+    {
+        var newPosition = clippingPlane.transform.localPosition;
+        newPosition.y = maxHeight * sliderEventData.NewValue;
+
+        clippingPlane.transform.localPosition = newPosition;
     }
 }
