6f94d9e14022775c48bc26f3d0ebe3e317aa45ba
Florian Jasche <florian.jasche@gmail.com>
2021-03-04 23:30:19 +0100

    Adjusted color of lines in graph.

--- ExtendedPrinter/Assets/DiagramDataController.cs
+++ ExtendedPrinter/Assets/DiagramDataController.cs
@@ -2,6 +2,7 @@
 using OctoPrintLib;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class DiagramDataController : MonoBehaviour
@@ -10,15 +11,31 @@ public class DiagramDataController : MonoBehaviour
     private GraphChart Graph;
 
 
+    [SerializeField]
+    private TextMeshProUGUI ToolTargetText;
+
+    [SerializeField]
+    private TextMeshProUGUI ToolActualText;
+    [SerializeField]
+    private TextMeshProUGUI BedTargetText;
+    [SerializeField]
+    private TextMeshProUGUI BedActualText;
+
     public void OnDataRecieved(CurrentMessage message)
     {
-        var tmps = message.current.temps;
-        if (tmps.Count > 0)
+        var temps = message.current.temps;
+        if (temps.Count > 0)
         {
-            Graph.DataSource.AddPointToCategoryRealtime("ToolTarget", System.DateTime.Now, (double)tmps[0].tool0.target, 1f); 
-            Graph.DataSource.AddPointToCategoryRealtime("ToolActual", System.DateTime.Now, (double)tmps[0].tool0.actual, 1f);
-            Graph.DataSource.AddPointToCategoryRealtime("BedTarget", System.DateTime.Now, (double)tmps[0].bed.target, 1f); 
-            Graph.DataSource.AddPointToCategoryRealtime("BedActual", System.DateTime.Now, (double)tmps[0].bed.actual, 1f);
+            Graph.DataSource.StartBatch();
+            Graph.DataSource.AddPointToCategoryRealtime("ToolTarget", System.DateTime.Now, (double)temps[0].tool0.target, 1f); 
+            Graph.DataSource.AddPointToCategoryRealtime("ToolActual", System.DateTime.Now, (double)temps[0].tool0.actual, 1f);
+            Graph.DataSource.AddPointToCategoryRealtime("BedTarget", System.DateTime.Now, (double)temps[0].bed.target, 1f); 
+            Graph.DataSource.AddPointToCategoryRealtime("BedActual", System.DateTime.Now, (double)temps[0].bed.actual, 1f);
+            Graph.DataSource.EndBatch();
+            ToolTargetText.text = temps[0].tool0.target.Value.ToString("F1");
+            ToolActualText.text = temps[0].tool0.actual.Value.ToString("F1");
+            BedTargetText.text = temps[0].bed.target.Value.ToString("F1");
+            BedActualText.text = temps[0].bed.actual.Value.ToString("F1");
         }
     }
 }
---
+++ ExtendedPrinter/Assets/PrusaPrintBedController.cs
@@ -0,0 +1,44 @@
+﻿using OctoPrintLib;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+public class PrusaPrintBedController : MonoBehaviour
+{
+    public Transform BuildPlate;
+
+    public void CurrentDataRecieved(CurrentMessage data)
+    {
+        //int i = data.current.logs.Count-1;
+        //while (i >= 0)
+        //{
+        //    //Send: N3848 G1 X69.186 Y86.257 E0.01178 * 84
+        //    if(data.current.logs[i].StartsWith("Send: "))
+        //    {
+        //        var yIndex = data.current.logs[i].IndexOf('Y');
+        //        if (yIndex >= 0)
+        //        {
+        //            var index = data.current.logs[i].IndexOf(' ', yIndex);
+        //            float yValue;
+        //            if (index >=0)
+        //            {
+        //               yValue = float.Parse(data.current.logs[i].Substring(yIndex + 1, index - yIndex), CultureInfo.InvariantCulture.NumberFormat);
+        //            }
+        //            else
+        //            {
+        //                yValue = float.Parse(data.current.logs[i].Substring(yIndex + 1), CultureInfo.InvariantCulture.NumberFormat);
+        //            }
+                      
+
+        //            float offset = 0.18f;
+
+        //            BuildPlate.localPosition = new Vector3(BuildPlate.localPosition.x, BuildPlate.localPosition.y, offset - (yValue / 1000f));
+        //            break;
+        //        }
+        //    }
+        //    i--;
+        //}
+                
+    }
+}
