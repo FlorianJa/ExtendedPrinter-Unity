74d6dd107d242c9965d4297d84a5a95b72b5ec57
Florian Jasche <florian.jasche@gmail.com>
2021-03-05 21:25:15 +0100

    Added buttons for controlling the printer

---
+++ ExtendedPrinter/Assets/PrinterControlsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.MixedReality.Toolkit.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrinterControlsController : MonoBehaviour
+{
+    public GameObject DialogPrefab;
+
+
+    public void OnPrinterStop()
+    {
+        Dialog myDialog = Dialog.Open(DialogPrefab,  DialogButtonType.No| DialogButtonType.Yes, "Druck abbrechen", "Möchtest du den Druck wirklich abbrechen?", true);
+        if (myDialog != null)
+        {
+            myDialog.OnClosed += OnClosedDialogEvent;
+        }
+    }
+
+    private void OnClosedDialogEvent(DialogResult obj)
+    {
+    }
+
+    public void OnPrinterPause()
+    {
+        
+    }
+}
