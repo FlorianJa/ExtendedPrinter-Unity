81ef7bb3ea82d8431166b2c375230f4232e6b028
Florian Jasche <florian.jasche@gmail.com>
2021-03-06 17:07:46 +0100

    Moved project folder.

---
+++ Assets/Scripts/UpdateInfillText.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.MixedReality.Toolkit.UI;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TextMeshPro))]
+public class UpdateInfillText : MonoBehaviour
+{
+
+    private TextMeshPro textMeshPro;
+
+    private void Start()
+    {
+        textMeshPro = GetComponent<TextMeshPro>();
+    }
+    public void UpdateText(SliderEventData data)
+    {
+        if(data != null && textMeshPro != null) textMeshPro.text = ((int)(data.NewValue*100)).ToString() + "%";
+    }
+}
