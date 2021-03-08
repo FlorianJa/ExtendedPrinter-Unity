1e84ed8208494ea06ada6764388398160b79f9c6
Florian Jasche <florian.jasche@gmail.com>
2021-03-08 22:01:24 +0100

    Enhanced the printing time example in the tutorial

---
+++ Assets/ScaleBenchy.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.MixedReality.Toolkit.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleBenchy : MonoBehaviour
+{
+    
+
+    public void ChangeScale(SliderEventData data)
+    {
+        int scaleIndex = (int)Math.Round(data.NewValue * 3f);
+
+        float scale = (100f + scaleIndex * 50)/100f;
+
+        transform.localScale = new Vector3(scale, scale, scale);
+    }
+}
